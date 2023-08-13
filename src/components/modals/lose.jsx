b770aefd1a158e8d10e618f0d402b0da202112d2
FlavioRoman <romanflavio2@gmail.com>
2023-08-13 00:18:21 -0400

    update

--- src/components/modals/lose.jsx
+++ src/components/modals/lose.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 const shadowStyleOn = {
   color: " #fff",
   textShadow: `0 0 7px #fff,
@@ -6,6 +8,9 @@ const shadowStyleOn = {
 };
 
 export const Lose = ({ resetAll }) => {
+  useEffect(() => {
+    setTimeout(() => resetAll(), 1000);
+  }, []);
   return (
     <section className="bg-[#25252590] flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 w-full h-full z-10 p-2">
       <article
@@ -16,13 +21,13 @@ export const Lose = ({ resetAll }) => {
           Mejor suerte la proxima
         </h1>
         <h2 className="text-center  text-[60px]">❌</h2>
-        <button
+        {/* <button
           type="button"
           onClick={() => resetAll()}
           className="border-slate-100 text-slate-100 border-solid border-2 rounded-lg font-semibold block mx-auto mt-4 p-2 transition-all hover:bg-slate-100 hover:text-slate-900"
         >
           PROBAR DE NUEVO
-        </button>
+        </button> */}
       </article>
     </section>
   );
